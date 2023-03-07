1f1c7da3adc6d7c972d909cee2576920e1fc043a
AlexVerm12 <alexandre.vermeersch@mtms.at>
2023-03-07 17:33:13 +0100

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,39 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="bg-gray-300 text-white w-1/2 m-auto mt-12">
+    <div class="text-center border-b-2 border-black-300 py-4">
+      <h1 class="text-3xl py-4">Unsere Todos</h1>
+      <p class="text-xl">Offene Todos: {{ todos.length }}</p>
+    </div>
+    <div v-for="(todo, index) in todos" :key="todo.todo">
+      <TodoList :todoprop="todo" :todoindex="index" @toggledone-index="toggleDone" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TodoList from "./components/TodoList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      todos: [
+        { todo: "Einkaufen", done: false },
+        { todo: "Sport", done: false },
+        { todo: "Programmieren", done: true },
+      ],
+    };
+  },
+  methods:{
+    toggleDone(index){
+      this.todos[index].done = !this.todos[index].done;
+    }
+  },
   components: {
-    HelloWorld
-  }
-}
+    TodoList,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex justify-between py-2 px-2" :class="{'bg-green-600': this.todo.done}">
+    <p @click="toggleTodo" class="text-xl cursor-pointer">{{ todo.todo }}</p>
+    <button class="bg-red-600 py-1 px-2 rounded">Delete</button>
+  </div>
+</template>
+<script>
+export default {
+  name: "TodoList",
+  props: ["todoprop", "todoindex"],
+  data() {
+    return {
+      todo: this.todoprop,
+    };
+  },
+  methods: {
+   toggleTodo(){
+    this.$emit("toggledone-index", this.todoindex)
+   }
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+</style>
